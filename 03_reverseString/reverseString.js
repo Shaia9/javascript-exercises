e1014169f15b258109cbb3fb462e34ac68a5f328
Shaia9 <Shaiabrett@gmail.com>
2023-07-22 23:00:27 -0500

    First Commit

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    return str;
 
 };
+reverseString("hello")
 
 // Do not edit below this line
 module.exports = reverseString;
